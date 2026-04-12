07832f659a58afadf48fe62e08f63ee3b397f5ac
Keshav-goyal006 <keshav.goyal@iitg.ac.in>
2026-04-12 17:07:07 +0530

    memory mapping

--- mem_generator/imem_dmem/bin2hex.py
+++ mem_generator/imem_dmem/bin2hex.py
@@ -1,4 +1,5 @@
 import struct
+import time
 MEMINIT = 1
 RAMSIZE = 4096
 
@@ -22,17 +23,39 @@ def bin2hex(in_file, out_file):
 			data += b'\x00' * (chunk - len(data))
 
 		data = struct.unpack("I",data)
-		outputFile.write(hexChange(str(hex(data[0]))) + " // 32'h" + hexChange(str(hex(j)))+ "\n")
+		outputFile.write(hexChange(str(hex(data[0]))) + "\n")
+		# outputFile.write(hexChange(str(hex(data[0]))) + " // 32'h" + hexChange(str(hex(j)))+ "\n")
 		j+=4
 	
 	while(j<RAMSIZE):
 		if (MEMINIT):
-			outputFile.write("00000000 // 32'h"+hexChange(str(hex(j)))+ "\n")
+			outputFile.write("00000000\n")
+			# outputFile.write("00000000 // 32'h"+hexChange(str(hex(j)))+ "\n")
 		else:
-			outputFile.write("xxxxxxxx // 32'h"+hexChange(str(hex(j))) + "\n")
+			outputFile.write("xxxxxxxx\n")
+			# outputFile.write("xxxxxxxx // 32'h"+hexChange(str(hex(j))) + "\n")
 		j+=4
 	inputFile.close()
 	outputFile.close()
 
+def hextomem(in_file, out_file):
+    with open(in_file, 'r') as f_in, open(out_file, 'w') as f_out:
+        # 1. The mandatory Xilinx address header
+        f_out.write("@00000000\n")
+        
+        for line in f_in:
+            # 2. Strip anything after // (the comments you showed me)
+            clean_line = line.split("//")[0].strip()
+            
+            # 3. Only write if the line isn't empty
+            if clean_line:
+                # updatemem expects the 8-character hex string as TEXT
+                f_out.write(clean_line + "\n")
+
+    print(f"Done! Created {out_file} for updatemem.")
+
 bin2hex("imem.bin", "imem.hex")
 bin2hex("dmem.bin", "dmem.hex")
+# time.sleep(10)
+hextomem("imem.hex", "imem.mem")
+hextomem("dmem.hex", "dmem.mem")
